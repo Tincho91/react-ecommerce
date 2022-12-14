a1885f61fa08879e13bf64038e7b3d3d638d8f7e
Martin Maiaru <tincho_91_15@hotmail.com>
2022-12-14 02:19:53 -0300

    Commit 1 - 14/12

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -5,31 +5,50 @@ import { useParams } from 'react-router-dom';
 import ItemList from './ItemList';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-
+import Spinner from 'react-bootstrap/Spinner';
 
 
 export const ItemListContainer = () => {
     const [datos, setDatos] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
     const { categoryId } = useParams(); 
-
+    
 
     useEffect(() => {
+        setIsLoading(true);
         if (categoryId) {
             customFetch(2000, itemDataBase.filter(item => item.categoryId == categoryId))
-            .then(response => setDatos(response))
+            .then(response =>  {
+                setDatos(response);
+                setIsLoading(false);
+            })
             .catch(error => console.log(error))
         } else {
             customFetch(2000, itemDataBase)
-            .then(response => setDatos(response))
+            .then(response =>  {
+                setDatos(response);
+                setIsLoading(false);
+            })
             .catch(error => console.log(error))
         }
     }, [categoryId])
 
+    if (isLoading) {
+        return(
+            <Container className="loadingContainer vh-100">
+                <Spinner className="loading" animation="grow" role="status" size='xl'> 
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            </Container>
+        )
+    };
+
     return(
         <Container className="itemListContainer">
             <Row>
-                <ItemList datos={datos}/>
+                <ItemList className="item-list" datos={datos}/>
             </Row>
         </Container>
     )
+    
 }
